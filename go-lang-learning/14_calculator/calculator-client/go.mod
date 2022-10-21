21e0d313aaebfe29a4a76ed2ca0311614c996386
S Rajesh <113459104+enstenr@users.noreply.github.com>
2022-10-21 18:11:52 +0530

    Add files via upload

---
+++ go-lang-learning/14_calculator/calculator-client/go.mod
@@ -0,0 +1,5 @@
+module calc-testing
+
+go 1.18
+
+require github.com/enstenr/go-mods v1.0.0 // indirect
---
+++ go-lang-learning/14_calculator/calculator-client/main.go
@@ -0,0 +1,6 @@
+package main 
+import (gomods "github.com/enstenr/go-mods")
+func main(){
+gomods.Add(1,2,"ADD")
+
+}

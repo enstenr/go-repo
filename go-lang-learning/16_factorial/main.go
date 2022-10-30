2c7338440e1ec0a3676a3fe1632dd690b278bc73
enstenr <enstenr@gmail.com>
2022-10-30 16:37:15 +0530

    gin framework

--- go-lang-learning/16_factorial/main.go
+++ go-lang-learning/16_factorial/main.go
@@ -4,11 +4,13 @@ import "fmt"
 
 func main() {
 	var num int64
+	fmt.Println("Enter the Number ")
 	fmt.Scanln(&num)
 
 	factorialValue := fact(num)
 	fmt.Println()
 	fmt.Printf(" Factorial of the Number is %d",factorialValue)
+	fmt.Println()
 }
 func fact(num int64) int64 {
 	if num == 0 {
